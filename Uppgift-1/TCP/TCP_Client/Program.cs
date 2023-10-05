b02d6aa8afda4ec3b4076360443e6c9e2901a339
Stefanie Schubert <stefanie.schubert@yh.nackademin.se>
2023-10-05 12:32:28 +0200

    update TCP

--- Uppgift-1/TCP/TCP_Client/Program.cs
+++ Uppgift-1/TCP/TCP_Client/Program.cs
@@ -1,34 +1,101 @@
-namespace TCP_Client
+using System.Net.Sockets;
+
+static void Connect()
 {
-    public class Program
+    string server = "127.0.0.1";
+    string message = "bajs";
+    try
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        // Create a TcpClient.
+        // Note, for this client to work you need to have a TcpServer
+        // connected to the same address as specified by the server, port
+        // combination.
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
+        Int32 port = 7281;
 
-            var app = builder.Build();
+        // Prefer a using declaration to ensure the instance is Disposed later.
+        using TcpClient client = new TcpClient(server, port);
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+        // Translate the passed message into ASCII and store it as a Byte array
+        Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+        // Get a client stream for reading and writing.
+        NetworkStream stream = client.GetStream();
 
-            app.UseRouting();
+        // Send the message to the connected TcpServer.
+        stream.Write(data, 0, data.Length);
 
-            app.UseAuthorization();
+        Console.WriteLine("Sent: {0}", message);
 
-            app.MapRazorPages();
+        // Receive the server response.
 
-            app.Run();
-        }
+        // Buffer to store the response bytes.
+        data = new Byte[256];
+
+        // String to store the response ASCII representation.
+        String responseData = String.Empty;
+
+        // Read the first batch of the TcpServer response bytes.
+        Int32 bytes = stream.Read(data, 0, data.Length);
+        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+        Console.WriteLine("Received: {0}", responseData);
+
+        // Explicit close is not necessary since TcpClient.Dispose() will be
+        // called automatically.
+        // stream.Close();
+        // client.Close();
     }
+    catch (ArgumentNullException e)
+    {
+        Console.WriteLine("ArgumentNullException: {0}", e);
+    }
+    catch (SocketException e)
+    {
+        Console.WriteLine("SocketException: {0}", e);
+    }
+
+    Console.WriteLine("\n Press Enter to continue...");
+    Console.Read();
 }
+
+
+
+
+//using Azure;
+//using System.Net.Sockets;
+//using System.Text;
+
+//class Client
+//{
+//	static void Main()
+//	{
+//		try
+//		{
+//			//connecting to server 8080
+//			TcpClient client = new TcpClient("127.0.0.1", 8080);
+//			NetworkStream stream = client.GetStream();
+
+//			//structur
+//			string request = "{\"action\": \"get_data\", \"parameter\": \"value\"}";
+
+//			//send data to server
+//			byte[] data = Encoding.UTF8.GetBytes(request);
+//			stream.Write(data, 0, data.Length);
+
+//			//answer from serverbyte
+//			byte[] buffer = new byte[1024];
+//			int bytesRead = stream.Read(buffer, 0, buffer.Length);
+//			string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+//			Console.WriteLine("Server answered" + response);
+
+//			stream.Close();
+//			client.Close();
+//		}
+//		catch (Exception ex)
+//		{
+//			Console.WriteLine("Error" + ex.Message);
+//		}
+//	}
+//}
+//}
--- Uppgift-1/TCP/TCP_Server/Program.cs
+++ Uppgift-1/TCP/TCP_Server/Program.cs
@@ -1,34 +1,50 @@
-namespace TCP_Server
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpServerExample
 {
-    public class Program
+    static void Main()
     {
-        public static void Main(string[] args)
+        // Set the TcpListener on port 8080.
+        Int32 port = 7281;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+        // Create a TcpListener to accept client connections
+        TcpListener server = new TcpListener(localAddr, port);
+        server.Start();
+        Console.WriteLine("Waiting for a connection...");
+
+        // Perform a blocking call to accept requests.
+        using TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected!");
+
+        // Get a stream object for reading and writing
+        using NetworkStream stream = client.GetStream();
+
+        byte[] bytes = new byte[256];
+        int i;
+
+        // Loop to receive all the data sent by the client.
+        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            // Translate data bytes to an ASCII string.
+            string data = Encoding.ASCII.GetString(bytes, 0, i);
+            Console.WriteLine("Received: {0}", data);
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
+            // Process the data sent by the client.
+            data = data.ToUpper();
 
-            var app = builder.Build();
+            byte[] msg = Encoding.ASCII.GetBytes(data);
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
+            // Send back a response.
+            stream.Write(msg, 0, msg.Length);
+            Console.WriteLine("Sent: {0}", data);
         }
+
+        // Shutdown and end connection
+        client.Close();
+        Console.WriteLine("Client disconnected");
     }
 }
