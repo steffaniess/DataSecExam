277c30f0c039a11c4f9443197c6830b577edc488
Stefanie Schubert <stefanie.schubert@yh.nackademin.se>
2023-10-05 14:53:58 +0200

    update WS in new folder

---
+++ Uppgift-3/WS/WS_Client/Classes/WebSocketClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WSClient.Classes
+{
+    public class WebSocketClient
+    {
+        static async Task Main()
+        {
+            var serverUri = new Uri("ws://localhost:8080");
+            var webSocket = new ClientWebSocket();
+
+            try
+            {
+
+                await webSocket.ConnectAsync(serverUri, CancellationToken.None);
+                Console.WriteLine("Connected to the WebSocket server...");
+
+                //Send data to server
+                var messageData = new { message = "Hi from client", timestamp = DateTime.Now };
+                var messageJson = Newtonsoft.Json.JsonConvert.SerializeObject(messageData);
+                var messageBytes = Encoding.UTF8.GetBytes(messageJson);
+
+                await SendWebSocketsMessage(webSocket, messageBytes);
+
+                //Receive data from server
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    var buffer = new byte[1024];
+                    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        var receivedData = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        Console.WriteLine($"Received {receivedData} from server");
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"WebSocket error: {ex.Message}");
+            }
+            finally
+            {
+                webSocket.Dispose();
+            }
+
+        }
+
+        static async Task SendWebSocketsMessage(WebSocket webSocket, byte[] message)
+        {
+            await webSocket.SendAsync(new ArraySegment<byte>(message), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+    }
+}
---
+++ Uppgift-3/WS/WS_Server/Classes/WebSocketServer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WSClient.Classes
+{
+    public class WebSocketServer
+    {
+        static async Task Main()
+        {
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8080/");
+            httpListener.Start();
+
+            Console.WriteLine("WebSocket server is running..");
+
+            while (true)
+            {
+                var context = await httpListener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest)
+                {
+                    var webSocketContext = await context.AcceptWebSocketAsync(null);
+                    var webSocket = webSocketContext.WebSocket;
+
+                    await HandleWebSocketConnection(webSocket);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
+                static async Task HandleWebSocketConnection(WebSocket webSocket)
+                {
+                    try
+                    {
+                        while (webSocket.State == WebSocketState.Open)
+                        {
+                            var buffer = new byte[1024];
+                            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                            if (result.MessageType == WebSocketMessageType.Text)
+                            {
+                                var receivedData = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                                Console.WriteLine($"Received: {receivedData}");
+
+                                //Stimulate the server to answer with Json-data
+                                var responseData = new { message = "Server response", timespan = DateTime.Now };
+                                //var responseJson = Newtonsoft.Json.JsonConvert.SerializeObject(responseData);
+                                var responseBytes = Encoding.UTF8.GetBytes(receivedData);
+
+                                await webSocket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"WebSocket error: {ex.Message}");
+                    }
+                    finally
+                    {
+                        webSocket.Dispose();
+                    }
+                }
+            }
+        }
+    }
+}
