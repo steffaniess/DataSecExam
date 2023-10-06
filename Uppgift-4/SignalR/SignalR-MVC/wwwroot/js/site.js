637c590c20ac8600049e3a28d64764965f725862
Stefanie Schubert <stefanie.schubert@yh.nackademin.se>
2023-10-06 17:20:37 +0200

    update js

--- Uppgift-4/SignalR/SignalR-MVC/wwwroot/js/site.js
+++ Uppgift-4/SignalR/SignalR-MVC/wwwroot/js/site.js
@@ -1,4 +1,3 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿//connection.on("ReceiveTemperature", (temperature) => {
+//    documet.getElementById('temperatureDisplay').innerText = 'Current temperature: ${temperature}°C';
+//});
