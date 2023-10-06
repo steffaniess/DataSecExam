30f7965a651dd6fc1ef7427c5452badc3f28c876
Stefanie Schubert <stefanie.schubert@yh.nackademin.se>
2023-10-06 09:12:48 +0200

    add MVC

---
+++ Uppgift-4/SignalR/SignalR-Client/Classes/IotDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR_Client.Classes
+{
+    public class IotDevice
+    {
+        private readonly string _hubUrl;
+
+        public IotDevice(string hubUrl)
+        {
+            _hubUrl = hubUrl;
+        }
+        public async Task ReportTemperatureAsync()
+        {
+
+        }
+    }
+}
---
+++ Uppgift-4/SignalR/SignalR-Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using SignalR_Client.Classes;
+
+namespace SignalR_Client
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var device = new IotDevice("https://localhost:7078/swagger/index.html");
+            await device.ReportTemperatureAsync();
+        }
+    }
+}
