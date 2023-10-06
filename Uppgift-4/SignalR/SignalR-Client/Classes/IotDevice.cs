35f4f4a172d1912fd0a32ab2ac0a350bbe7c168f
Stefanie Schubert <stefanie.schubert@yh.nackademin.se>
2023-10-06 12:08:13 +0200

    update Iot-class (client)

--- Uppgift-4/SignalR/SignalR-Client/Classes/IotDevice.cs
+++ Uppgift-4/SignalR/SignalR-Client/Classes/IotDevice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +10,33 @@ namespace SignalR_Client.Classes
     public class IotDevice
     {
         private readonly string _hubUrl;
+        private readonly Random _random = new Random();
+        private HubConnection _hubConnection;
+
 
         public IotDevice(string hubUrl)
         {
             _hubUrl = hubUrl;
         }
+        public async Task InitializeConnectionAsync()
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(_hubUrl)
+                .Build();
+        }
         public async Task ReportTemperatureAsync()
         {
-
+            if (_hubConnection == null)
+            {
+                await InitializeConnectionAsync();
+            }
+             
+            while (true) 
+            {
+                var temperature = _random.Next(-20, 50);
+                await _hubConnection.SendAsync("SendTemperature", temperature.ToString());
+                await Task.Delay(5000); //varje rapport uppdateras var 5e min
+            }
         }
     }
 }
